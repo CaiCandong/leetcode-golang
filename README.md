8a24da5967d3fb243e068110ca4791775ecf9822
caicandong <1290147055@qq.com>
2020-02-28 09:44:37 +0800

    20 有效的括号

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # leetcode in golang  
 ## 问题列表
-- [x] 8.字符串转换整数 (atoi)    
+- [x] [8.字符串转换整数 (atoi)](https://github.com/CaiCandong/leetcode-golang/tree/master/code/problem0008)
+- [x] [20.有效的括号](https://github.com/CaiCandong/leetcode-golang/tree/master/code/problem0020)
---
+++ code/problem0020/isValid.go
@@ -0,0 +1,31 @@
+package problem0020
+
+func isValid(s string) bool {
+	if s == "" {
+		return true
+	}
+	stack := make([]int32, 0)
+	for _, char := range s {
+		switch {
+		case len(stack) == 0:
+			stack = append(stack, char)
+		case !isMatch(stack[len(stack)-1], char):
+			stack = append(stack, char)
+		case isMatch(stack[len(stack)-1], char):
+			stack = stack[:len(stack)-1]
+		}
+	}
+	return len(stack) == 0
+}
+func isMatch(left, right int32) bool {
+	switch {
+	case left == '(' && right == ')':
+		return true
+	case left == '[' && right == ']':
+		return true
+	case left == '{' && right == '}':
+		return true
+	default:
+		return false
+	}
+}
